626e59752b2cd79e53a6b95880488e8c542dc9bd
Ahmed kallel <113143693+ahmede9ka@users.noreply.github.com>
2024-12-12 22:02:39 +0100

    dashboard

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { PersonaLayoutComponent } from './layout/persona-layout/persona-layout.component';
 import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
             {
                 path: '',
                 component: LandingPageComponent,
+            },
+            {
+                path: 'dashboard',
+                component: DashboardComponent,
             }
         ]
     }
--- Frontend/src/app/views/dashboard/dashboard-routing.module.ts
+++ Frontend/src/app/views/dashboard/dashboard-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Chart, registerables } from 'chart.js';
 
 const routes: Routes = [];
 
@@ -7,4 +8,24 @@ const routes: Routes = [];
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class DashboardRoutingModule { }
+export class DashboardComponent {
+  constructor() {
+    Chart.register(...registerables);
+  }
+
+  ngAfterViewInit() {
+    new Chart('pieChart', {
+      type: 'pie',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+          {
+            label: 'Dataset 1',
+            data: [30, 50, 20],
+            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          }
+        ]
+      }
+    });
+  }
+}
